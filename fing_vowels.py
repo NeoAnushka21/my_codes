8f7d1844bbfe3b3efe93409e89fd761a8899de61
NeoAnushka21 <99464616+NeoAnushka21@users.noreply.github.com>
2022-03-23 11:28:25 +0530

    Delete fing_vowels.py

--- fing_vowels.py
+++
@@ -1,9 +0,0 @@
-file01 = open("python_intro.txt", "r")
-read_file = file01.read()
-print(read_file)
-
-regex = "aeiou"
-for i in read_file:
-    i.lower()
-    if i in regex:
-        print(i, end=" ")
