437ee1a3c0e537f1ffaa0a2b06050669e02a28a1
Neosoft <anushkamhatre2707@gmail.com>
2022-04-11 19:19:25 +0530

    updated

--- leetcode_add_two_numbers.py
+++ leetcode_add_two_numbers.py
@@ -1,18 +1,19 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+import self as self
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+    self.val = val
+    self.next = next
+
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    def add_two_numbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
         temp01 = l1                  # temp variable for 1st list
         temp02 = l2                  # temp variable for 2nd list
-
         addition = ListNode()        # List to store sum
-
         temp03 = addition            # temp variable to loop through addition
-
         carry = 0
 
         while temp01 is not None or temp02 is not None:  # condition given to continue until both l1 and l2 become None
@@ -22,11 +23,16 @@ class Solution:
             carry = s // 10
             temp03.next = ListNode(s % 10)  # storing the Units place of s in list
             temp03 = temp03.next
+
             if temp01 is not None:
                 temp01 = temp01.next
             if temp02 is not None:
-                temp2 = temp02.next
+                temp02 = temp02.next
+
         if carry > 0:
             temp03.next = ListNode(carry)  # add the last node only if the carry over is 0 ie no carry
 
         return addition.next  # since head was dummy
+
+obj = Solution()
+print(obj.add_two_numbers([2,3,5],[1,3,4]))
--- leetcode_two_sum.py
+++ leetcode_two_sum.py
@@ -1,12 +1,12 @@
 class Solution:
 
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def two_sum(self, nums: [int], target: int) -> [int]:
 
         for i in range(len(nums) - 1):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    return ([i, j])
+                    print(([i, j]))
 
 
 obj = Solution()
-obj.twoSum([2, 7, 11, 15], 9)
+obj.two_sum([2, 7, 11, 15], 17)
